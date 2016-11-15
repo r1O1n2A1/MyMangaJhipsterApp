2e9e069e5731c42f2dec39ee2769c9001ec943ae
ronan <ronan.lachater@gmail.com>
2016-11-15 23:23:55 +0100

    commit main

---
+++ com.manga.app/src/main/java/com/manga/MyMangaApp.java
@@ -0,0 +1,13 @@
+package com.manga;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MyMangaApp {
+	private static Logger log = LoggerFactory.getLogger(MyMangaApp.class);
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
